85f38d90c787ea31e2b917ad586418c65746232f
LAUBERGINE <gamer.8300.yt@gmail.com>
2024-05-06 11:32:13 +0200

    Detect dll/so

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from recursif_dependencies import *
 
 LIB_BASE = "LibPlants.so"
-index_point = LIB_BASE.find('.')
-if index_point != -1:
-    apres_point = LIB_BASE[index_point + 1:]
+extension = LIB_BASE.find('dll')
 all_dependencies = {}
 
-if apres_point == "dll":
+if extension >= 1:
     process_dependencies_windows(LIB_BASE, all_dependencies)
 else:
     process_dependencies_linux(LIB_BASE, all_dependencies)
